cd1e2ce79fd20e8e7f56fd4038a21038b09dec13
Ashley Cruz <ashleynayellicruz@gmail.com>
2022-01-26 01:48:04 -0600

    google access verification modified

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -1,5 +1,5 @@
 const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_SECRET_ID, process.env.REDIRECTURI);
 
 const googleVerify = async(tokenId) => {
 
